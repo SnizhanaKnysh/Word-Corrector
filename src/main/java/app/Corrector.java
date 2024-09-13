b2b99b3bd4ab96e89f5ddd0a6e227c52b74ad0a6
Snizhana_K <snizhana.knysh@intellecteu.com>
2024-09-13 14:49:10 +0300

    add word corrector files

---
+++ src/main/java/app/Corrector.java
@@ -0,0 +1,23 @@
+package app;
+
+public class Corrector {
+
+    public String handleData(String[] strs) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int count = 1;
+
+        for (String str : strs) {
+            if (str.contains("b")) {
+                str = str.replace("b", "o");
+            }
+
+            if (str.contains("nibn")) {
+                str = str.replace("nibn", "onion");
+            }
+            stringBuilder.append(count).append(") ").append(str).append("\n");
+            count++;
+        }
+        return stringBuilder.toString();
+    }
+}
